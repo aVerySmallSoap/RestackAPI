2586987a2dfc0427143f5fd255326fb4784a7ce6
Lirys <iamyourmarkieboo@gmail.com>
2025-06-09 21:47:59 +0800

    Added new dependencies from external scripts

--- modules/utils/install_dependencies.py
+++ modules/utils/install_dependencies.py
@@ -1,1 +1,20 @@
-#TODO: Add new dependencies of fastAPI and OLD CODE
+import subprocess
+import sys
+
+subprocess.check_call([
+    sys.executable,
+    "-m",
+    "pip",
+    "install",
+    "wapiti3",
+    "Flask",
+    "flask-cors",
+    "tz",
+    "SQLAlchemy",
+    "SQLAlchemy-Utils",
+    "psycopg2",
+    "bs4",
+    "beautifulsoup4",
+    "requests",
+    "ijson",
+])
