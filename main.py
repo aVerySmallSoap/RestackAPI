7932a0e8548fd532cecee3134fa86b018f1bb8b6
Lirys <iamyourmarkieboo@gmail.com>
2025-06-09 21:56:24 +0800

    Added basic api operations to run web app

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+)
+
+@app.get("/api/v1/wapiti/scan")
+async def wapiti_scan(url: str):
+    pass
+
+@app.get("/api/v1/wapiti/report/{report_id}")
+async def wapiti_report(report_id: str):
+    pass
+
+if __name__ == '__main__':
+    pass
