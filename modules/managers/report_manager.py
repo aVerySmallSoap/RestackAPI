daf4430fb45c96b7401039f990db94740efdc0bb
Lirys <iamyourmarkieboo@gmail.com>
2025-07-04 04:32:54 +0800

    File structure changes

--- modules/managers/report_manager.py
+++ modules/managers/report_manager.py
@@ -2,7 +2,7 @@
 
 class ReportManager:
     """Manages the naming and local paths of generated reports."""
-    _local_path:str = "./reports/" # Reference path in which reports are stored
+    _local_path:str = "../../reports" # Reference path in which reports are stored
     _reports:list = [] # This array should only store a dict that has two values, path and name, wherein name is the name of the report
     pointer:int = 0 # points to the current report being watched | temporary db solution
 
