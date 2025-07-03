75e5dfca1d0a0e179d55bd3feaa5ce887e7ffaf5
Lirys <iamyourmarkieboo@gmail.com>
2025-07-04 04:32:27 +0800

    Added a skeleton class for docker

---
+++ modules/managers/docker_manager.py
@@ -0,0 +1,10 @@
+class DockerManager:
+    """Manages docker containers and local file paths of reports."""
+    _path_whatweb = "../../temp/whatweb/"
+    _path_zap = ""
+    _path_arachni = ""
+
+    def __init__(self):
+        pass
+
+    
