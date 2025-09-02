97a072cbf8c7f0cbc08ab4c7839d8b38b3341806
Lirys <iamyourmarkieboo@gmail.com>
2025-09-02 12:33:44 +0800

    search_vuln integration
    - Added support for search_vuln
    - Added basic reporting for search_vuln
    - Added parsing for WhatWeb results

--- modules/utils/docker_utils.py
+++ modules/utils/docker_utils.py
@@ -28,7 +28,27 @@ def update_vuln_search_service():
     client = docker.from_env()
     client.containers.run("search_vulns", auto_remove=True, tty=True, command=["./search_vulns.py", "-u"])
 
-def vuln_search_query(technology: str|list[str]):
+def vuln_search_query(technology: str|list[dict]):
     """Queries vulnerabilities found in a fingerprinted technology.
     :param technology: a technology or list of technologies"""
-    pass
+    _temp = []
+    _commands = ["./search_vulns.py", "--include-single-version-vulns", "-f", "json", "-o", "/home/search_vulns/reports/report.json"]
+    for dictionary in technology:
+        for key, value in dictionary.items():
+            if type(value) is list: # multiple versions detected
+                for version in value:
+                    _temp.append(f"{key} {version}")
+            else:
+                _temp.append(f"{key} {value}")
+    for query in _temp:
+        _commands.append("-q")
+        _commands.append(query)
+
+    client = docker.from_env()
+    client.containers.run(
+        "search_vulns",
+        auto_remove=True,
+        tty=True,
+        volumes={"D:\\Coding_Projects\\Python\\RestackAPI\\temp\\search_vulns": {"bind": "/home/search_vulns/reports","mode": "rw"}},
+        command=_commands,
+    )
--- tests/whatweb_parse_test.py
+++ tests/whatweb_parse_test.py
@@ -1,7 +1,4 @@
 import json
-
-# Excluded in report: Script, UncommonHeaders, Open-Graph-Protocol, Title, Frame
-
 _excluded = ["UncommonHeaders", "Open-Graph-Protocol", "Title", "Frame", "Script"]
 _trivial = ["Email", "Script", "IP", "Country", "HTTPServer"]
 _versioned_tech = []
@@ -9,21 +6,38 @@ _tech = []
 _cookies = []
 _extra = []
 
+def _parse_meta_generator(meta_data: dict):
+    for item in meta_data:
+        _string = ""
+        _version = ""
+        for index in range(len(item)):
+            if item[index] == ";": # Edge case of Tech_name version; wherein the tech is displayed with features
+                break
+            if item[index].isdigit() or item[index] == ".":
+                _version += item[index]
+            elif item[index] != len(item) - 1:
+                _string += item[index]
+        _versioned_tech.append({_string.rstrip(): [_version]})
+
+# TODO: Look up _versioned_tech in search_vulns docker
 with open("../temp/whatweb/report.json") as report:
     report = json.load(report)
 
     for plugin, content in report[0]["plugins"].items():
+        if plugin == "MetaGenerator" and len(content) > 0:
+            _parse_meta_generator(content["string"])
+            continue
         if plugin in _excluded:
             continue
         if plugin in _trivial:
-            _extra.append({plugin: content})
+            _extra.append({plugin: content["string"]})
             continue
         if plugin == "Cookies": # Handle cookies differently
-            _cookies.append(content)
+            _cookies.append({plugin: content["string"]})
+            continue
         if content is not None and "version" in content:
-            _versioned_tech.append({plugin: content["version"]})
+            _temp = {plugin: content["version"]}
+            if _temp not in _versioned_tech:
+                _versioned_tech.append(_temp)
             continue
         _tech.append({plugin: content})
-        # print(f"{plugin}: {content}")
-
-    print(f"Versioned Technologies:\n{_versioned_tech}\n\nTechnologies:\n{_tech}\n\nExtra:\n{_extra}\n\nCookies:\n{_cookies}")
