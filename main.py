5356a37525bf1904b42602eb052ecb752021a4c7
Lirys <iamyourmarkieboo@gmail.com>
2025-10-30 23:09:41 +0800

    Allowed the api to return the results on swagger for testing

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, AnyUrl
 
 from modules.db.database import Database
-from modules.db.filters.filter_by_date import date_filter_range
 from modules.interfaces.enums.ScanTypes import ScanType
 from modules.interfaces.enums.ZAPScanTypes import ZAPScanTypes
 from modules.interfaces.enums.ScannerTypes import ScannerTypes
@@ -19,6 +18,7 @@ from modules.utils.check_dir import check_directories
 
 # == TESTING MODULES ==
 from modules.utils.DEV_utils import check_url_local_test
+
 # == END OF TESTING MODULES==
 
 # == TEST WEBSITES ==
@@ -40,7 +40,7 @@ app.add_middleware(
 )
 
 class ScanRequest(BaseModel):
-    url:AnyUrl
+    url: AnyUrl
 
 # TODO: Check if reports folder is present in CWD
 # NOTE! THIS SHOULD BE DEVELOPED WITH SPECIALIZATION IN MIND. THIS APP SHOULD NOT BE GENERALIZED TO SPEED UP DEVELOPMENT
@@ -59,7 +59,7 @@ async def wapiti_scan(request: ScanRequest) -> dict:
     path = _scannerEngine.generate_file(ScannerTypes.WAPITI)
     config = _wapiti_scanner.generate_config({"path": path, "modules": ["all"]})
     _wapiti_scanner.start_scan(_URL, ScanType.QUICK, config)
-    report = _wapiti_scanner.parse_to_sarif(path) # TODO: Change how the DB reads this
+    report = _wapiti_scanner.parse_to_sarif(path)  # TODO: Change how the DB reads this
     await _whatweb_scanner.start_scan(_URL)
     _whatweb_results = _whatweb_scanner.parse_results()
     time_end = time.perf_counter()
@@ -70,9 +70,11 @@ async def wapiti_scan(request: ScanRequest) -> dict:
     else:
         return {"data": report, "plugins": _whatweb_results, "scan_time": scan_time}
 
+
 @app.post("/api/v1/wapiti/scan/full")
 async def wapiti_scan_full(request: ScanRequest) -> dict:
     pass
+
 
 @app.post("/api/v1/zap/scan/passive")
 async def zap_passive_scan(request: ScanRequest) -> dict:
@@ -91,12 +93,12 @@ async def zap_passive_scan(request: ScanRequest) -> dict:
     report = _zap_scanner.parse_results(path)
     time_end = time.perf_counter()
     scan_time = time_end - time_start
-    # if not _whatweb_results.__contains__("error"):
+    if not _whatweb_results.__contains__("error"):
     #     _db.insert_zap_report(_scan_start, path, _whatweb_results["raw"], report, scan_time)
-    #     return {"data": report, "plugins": _whatweb_results["data"], "scan_time": scan_time}
-    # else:
+        return {"data": report, "plugins": _whatweb_results["data"], "scan_time": scan_time}
+    else:
     #     _db.insert_zap_report(_scan_start, path, _whatweb_results["raw"], report, scan_time)
-    #     return {"data": report, "plugins": _whatweb_results, "scan_time": scan_time}
+        return {"data": report, "plugins": _whatweb_results, "scan_time": scan_time}
 
 
 @app.post("/api/v1/zap/scan/active")
@@ -116,37 +118,9 @@ async def zap_active_scan(request: ScanRequest) -> dict:
     report = _zap_scanner.parse_results(path)
     time_end = time.perf_counter()
     scan_time = time_end - time_start
-    # if not _whatweb_results.__contains__("error"):
+    if not _whatweb_results.__contains__("error"):
     #     _db.insert_zap_report(_scan_start, path, _whatweb_results["raw"], report, scan_time)
-    #     return {"data": report, "plugins": _whatweb_results["data"], "scan_time": scan_time}
-    # else:
+        return {"data": report, "plugins": _whatweb_results["data"], "scan_time": scan_time}
+    else:
     #     _db.insert_zap_report(_scan_start, path, _whatweb_results["raw"], report, scan_time)
-    #     return {"data": report, "plugins": _whatweb_results, "scan_time": scan_time}
-
-@app.get("/api/v1/wapiti/report/{report_id}")
-async def wapiti_report(report_id: str) -> dict:
-    pass
-
-@app.get("/api/v1/history/fetch")
-async def history_fetch():
-    pass
-
-@app.get("/api/v1/report/{report_id}")
-async def report_fetch(report_id: str):
-    pass
-
-@app.get("/api/v1/filter/reports/range/")
-async def filter_date_by_range(start:str, end:str):
-    return date_filter_range(_db.engine, start, end)
-
-@app.get("/api/v1/filter/reports/week/")
-async def filter_date_by_month(date:str):
-    pass
-
-@app.get("/api/v1/filter/reports/month/")
-async def filter_date_by_month(month:int):
-    pass
-
-@app.get("/api/v1/filter/reports/year/")
-async def filter_date_by_year(year:int):
-    pass
+        return {"data": report, "plugins": _whatweb_results, "scan_time": scan_time}
